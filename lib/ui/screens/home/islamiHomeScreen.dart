4b96fe368e16f00398b0cb032f3ff56ea9889080
ahmadtamers <ahmadtamerats@gmail.com>
2023-11-01 20:47:23 +0200

    put icons and colors variables

---
+++ lib/ui/screens/home/islamiHomeScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class IslamiHomeScreen extends StatelessWidget { 
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
---
+++ lib/ui/utilities/islamiAppColors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class IslamiAppColors{
+
+  static const Color primary = Color(0xFFB7925F);
+  static const Color accent = Color(0x242424);
+  static const Color white = Colors.white;
+  static const Color transparent = Colors.transparent;
+}
