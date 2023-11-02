6f9c55a33c07588be1ea18af03a4516058487f3d
ahmadtamers <ahmadtamerats@gmail.com>
2023-11-02 12:06:38 +0200

    QuranTab without open new page, when click on Sura

--- lib/ui/screens/home/tabs/quran_tab/quran_tab.dart
+++ lib/ui/screens/home/tabs/quran_tab/quran_tab.dart
@@ -1,8 +1,50 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:islami_app_ahmad_tamer/ui/utilities/islamiAppColors.dart';
+import 'package:islami_app_ahmad_tamer/ui/utilities/islamiAppIcons.dart';
+import 'package:islami_app_ahmad_tamer/ui/utilities/islamiAppTheme.dart';
+import 'package:islami_app_ahmad_tamer/ui/utilities/islamiConstants.dart';
 
 class Quran_tab extends StatelessWidget {  
   @override
   Widget build(BuildContext context) {
-    return Text("Quran");
+    return 
+      Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Image.asset(IslamiAppIcons.quran_logo)
+          ),
+          Expanded(
+            flex: 7,
+            child: 
+              Column(
+                children: [
+                  Divider(
+                    color: IslamiAppColors.primary,
+                    thickness:3,
+                  ),
+                  Text("Sura Name",style: IslamiAppTheme.islamiQuraTabSuraName.copyWith(color: IslamiAppColors.accent),),
+                  Divider(
+                    color: IslamiAppColors.primary,
+                    thickness:3,
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: IslamiConstants.Suraname.length,
+                      itemBuilder: (_,index){
+                        return 
+                          TextButton(
+                            onPressed: (){},
+                            child: Text(IslamiConstants.Suraname[index],textAlign: TextAlign.center,style: IslamiAppTheme.islamiQuraTabSuraName.copyWith(color: IslamiAppColors.accent),)
+                          );
+                      }),
+                  )
+                ],
+              )
+          ),
+
+
+        ],
+      );
   }
 }
--- lib/ui/utilities/islamiAppTheme.dart
+++ lib/ui/utilities/islamiAppTheme.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 abstract class IslamiAppTheme{
 
   static const TextStyle islamiAppBarFont = TextStyle(fontSize: 30 ,fontWeight: FontWeight.bold);
+  static const TextStyle islamiQuraTabSuraName = TextStyle(fontSize: 25 ,fontWeight: FontWeight.w600);
 
 }
