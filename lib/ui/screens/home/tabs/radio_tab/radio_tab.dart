902b979355bca60d658e5e20c83ff71ce5e05d49
ahmadtamers <ahmadtamerats@gmail.com>
2023-11-03 14:30:14 +0200

    radio shape

--- lib/ui/screens/home/tabs/radio_tab/radio_tab.dart
+++ lib/ui/screens/home/tabs/radio_tab/radio_tab.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app_ahmad_tamer/ui/utilities/islamiAppIcons.dart';
 
 class Radio_tab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text("Radio") ;
+    return
+      Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                flex: 4,
+                child: 
+                   Image.asset(IslamiAppIcons.radio_logo)
+                  
+              ),
+              Spacer(
+                flex: 6,
+              ),
+      
+            ]);
   }
 }
